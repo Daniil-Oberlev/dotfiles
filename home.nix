5dc963ee6b564150164649a4774461a16d6ce655
Daniil Oberlev <denioberlev@gmail.com>
2025-08-08 21:42:23 +0300

    refactor(config): delete extra hints

--- home.nix
+++ home.nix
@@ -11,8 +11,5 @@
   home.username = "chouqe";
   home.homeDirectory = "/home/chouqe";
 
-  # Перезагружать службы при изменении home.nix
-  systemd.user.startServices = "sd-switch";
-
   home.stateVersion = "25.05";
 }
